940a366af5636ecaa61488c308e50e7d1853fac4
Jams Lin <james@cardinalpoints.com.hk>
2023-07-26 18:40:00 +0800

    edited DB set up

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load()
 }
@@ -12,6 +14,23 @@ const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
 
+//setting up database connection
+const mongoose = require('mongoose')
+const username = process.env.DB_USERNAME
+const password = process.env.DB_PASSWORD
+const cluster = process.env.DB_CLUSTER
+const database = process.env.DB_DATABASE
+console.log(process.env);
+const mongoDBUrl = `mongodb+srv://${username}:${password}@${cluster}/${database}?retryWrites=true&w=majority`
+mongoose.connect(mongoDBUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+//setting up pages
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
@@ -19,12 +38,6 @@ app.use(expressLayouts)
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
